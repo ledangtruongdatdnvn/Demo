39dd05ed2b4dc3d84898c1eacb72a6e3eb8828d3
Lê Đặng Trường Đạt <ledangtruongdatdnvn@gamil.com>
2021-03-21 14:47:23 +0700

    add file main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  re
+//
+//  Created by Leo Lee on 21/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
